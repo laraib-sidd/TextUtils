417dcaa05592d06b345d1be8f84675db0edbf2c4
Laraib siddiqui <34001535+laraib-sidd@users.noreply.github.com>
2021-10-20 16:04:10 +0530

    Update no.of lines.py
    Added a new approach

--- no.of lines.py
+++ no.of lines.py
@@ -3,3 +3,9 @@ s=myfile.readlines()
 linecount=len(s)
 print("No.of lines in Poem.txt",linecount)
 myfile.close()
+
+# Optimized way to that
+with open("C:\Users\pc\Desktop\Python programs\Poem.txt",'r') as fs:
+  s = fs.realines()
+  print(f"Number of lines in poem.txt are {len(s)}")
+  
